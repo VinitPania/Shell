5dcfca4f0e2e6e3469f3f2251101378775e68aa0
VinitPania <vinit.pania@cloverinfotech.com>
2023-04-17 17:43:25 +0530

    Global Variable in Action

--- local-variable.sh
+++ local-variable.sh
@@ -1,3 +1,21 @@
-#!bin/bash
+#!/bin/bash
 # the script is a global and we can access it from any where 
-#Local variabe is the system has ascsbn ;dsjfvkn fjd 
+#Local variabe is the system it has scope inside of a function we use local keyword to define a local variable 
+
+
+
+#For example 
+function print (){
+    name=$1 #the $name  is global since we havent defined  the variable as local 
+    echo "$name is this in function "  
+}
+
+name="Boi"
+
+echo "Name is  $name :Before Hellow Function"
+
+print Moi
+
+echo "Name is $name  :After Hellow Function "
+
+
