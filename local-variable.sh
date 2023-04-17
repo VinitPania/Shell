1170d83d393321837d360c9041875252c5436dd9
VinitPania <vinit.pania@cloverinfotech.com>
2023-04-17 17:58:40 +0530

    local variable in action

--- local-variable.sh
+++ local-variable.sh
@@ -6,16 +6,37 @@
 
 #For example 
 function print (){
-    name=$1 #the $name  is global since we havent defined  the variable as local 
+    name=$1 #the name  is global since we havent defined  the variable as local 
     echo "$name is this in function "  
 }
 
-name="Boi"
 
-echo "Name is  $name :Before Hellow Function"
+#Now we create another function that has local variable 
 
-print Moi
+function printlocal (){
 
-echo "Name is $name  :After Hellow Function "
+    local system=$1
+    echo "I am printing local variable $system"
+}
+
+
+name="Boi" #This  name is also  a global variable 
+
+echo "Name is  $name:Before Hellow Function"
+
+print Moi #This function will override the previous name and add the Moi to Boi after 
+#the echo below will print Moi since name variable is global 
+echo "Name is $name :After Hellow Function "
+
+
+#Declared the global variable  as system
+system=ubuntu
+echo "Printing global variable as ->  $system "
+#calling the local variable function as mac
+printlocal Macos
+
+echo "printing the variable -> $system " #it will not print local it will print global
+
+
 
 
