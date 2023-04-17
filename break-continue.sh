add763fcd283674e1a5fd758b02bca5e1be632c1
VinitPania <vinit.pania@cloverinfotech.com>
2023-04-17 11:40:07 +0530

    Now the continue statment will work

--- break-continue.sh
+++ break-continue.sh
@@ -6,6 +6,8 @@ for (( i=1; i<20; i++ )); do
         echo "Break loop"
         break
         #continue keyword not working in this script 
+
+        #now its working 
     
     fi
     
@@ -26,3 +28,20 @@ for (( i=1; i<10; i++ )); do
 done 
 
 
+for (( i=1; i<20 ; i++ ));do 
+    if [ $i -eq 3 -o $i -eq 12 ]
+    then 
+        echo "Skipping the 3 and 12 "
+       
+        continue
+    fi
+     
+    if [ $i -eq 14 ]
+    then 
+        echo "Breaking the loop"
+        break
+    fi 
+    
+    echo $i
+
+done
