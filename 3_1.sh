e4575b4255d663e14e1fd8d2c663ffc0906763b9
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-09 18:49:40 +0530

    Word boundary n pipe

--- 3_1.sh
+++ 3_1.sh
@@ -43,3 +43,47 @@ echo "$line1"
 echo 
 echo "Second Line in $File is:"
 echo "$line2"
+
+#Example Saving the output of a code block to a file 
+SUCCESS=0
+E_NOARGS=65
+
+if [ -z "$1" ]
+then 
+    echo "Usage : `basename $0` rpm-file"
+    exit $E_NOARGS
+fi 
+
+{ #Begin of code block 
+    echo 
+    echo "Archive Description"
+    rpm -qpi $1   #Query Description.
+    echo 
+    echo "Archive Listing:"
+    rpm -qpi $1 #Query listining
+    echo 
+    rpm -i --test $1 #Query weather rpm files can be installed 
+    if [ "$?" -eq $SUCCESS  ]
+    then
+        echo "$1 can be installed"
+    else
+        echo "$1 cannot be installed"
+    fi
+    echo
+
+} > $1.test
+
+echo "Result of rpm test in file $1.test"
+
+
+# Example of ASCII Comparison 
+veg1=carrots
+veg2=tomatoes
+if [[ "$veg1" < "$veg2" ]]
+then 
+    echo "Although $veg1 precede $veg2 in the dictionary,"
+    echo -n "this does not necessarily imply anything "
+    echo "about my culinary preferences."
+else
+    echo "What kind of dictionary are you using, anyhow?"
+fi
