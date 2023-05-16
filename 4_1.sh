f8c3021574cdc51070f315df4898d776708cc328
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-16 18:48:01 +0530

    Added some code

--- 4_1.sh
+++ 4_1.sh
@@ -9,3 +9,6 @@
 variable1=121
 echo variable1 #Calling the name of the variable 
 echo $variable1 #Referance  the name of the variable 
+
+# The only time the variable is without prefix is when it is declared or assigned.
+#When exported in an in an arithmetic expression within double parentheses((...)).
