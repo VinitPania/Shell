337e2d4e7065a926e3442ef13b93b1d68e6fd502
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-16 10:50:30 +0530

    Added the description of the variable with example

---
+++ 4_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Variable Substitution 
+
+#The name variable is a placeholder for its value , the data it holds.
+#Referencing its value is called "variable subustitution"
+# To distinguish between the name of the variable and its value .
+#If the variable1 is the name of the variable , then the $variable 
+#is the reference of its value, the data item it conatain. 
+variable1=121
+echo variable1 #Calling the name of the variable 
+echo $variable1 #Referance  the name of the variable 
