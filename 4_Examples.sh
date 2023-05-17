2db155154e7cc64bbf6ab8cace50486f070fec29
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-17 12:07:04 +0530

    Added more to 4_Examples

--- 4_Examples.sh
+++ 4_Examples.sh
@@ -37,3 +37,51 @@ echo
 echo '$hello'  #$hello
 
 # Variable referencing diabled (escaped) by the single quotes, Which causes the "$" to be interpreted literally.
+
+#------------------------------------------------------------
+
+# It is permissible to set multiple variables on the same line, if serperated by white space 
+#  Caution , this may reduce legibility , and may not be portiable.
+
+var1=232 var2=3233 var3=$V3
+echo
+echo "var1=$var1 var2=$var2 var3=$V3"
+
+#May cause problen with leagacy version of "sh"
+# -------------------------------------------------
+
+echo; echo
+number="one two three"
+#          ^   ^
+other_numbers="1 2 3"
+#               ^ ^
+#If the whitespaces embeded within the variable the quotes are necessary.
+# other_numbers=1 2 3 ----> gibes error message 
+echo "numbers = $number"
+echo "other_numbers = $other_numbers" #other_numbers=1 2 3 
+
+# Escaping the whitespace also works 
+mixed_bags=1089\ ----\ Vegito
+#               ^     ^   Spaces after escape (\).
+echo "$mixed_bag"     #1089 ---- Vegito
+echo; echo
+
+
+
+# -----------------------------------------------------------
+
+#Set and Unset 
+
+echo "uninitiallized_variable = $uninitialized_variable"
+#Uninitialized variable has a null value (no value at a;lllllllll!).
+
+uninitialized_variable= #Declaring , but not initializing it same as setting it to a null value, as above,
+
+echo "uninitialized_variable = $uninitialized_variable"
+#It still has a null value 
+
+uninitialized_variable=1313  #set it 
+unset uninitialized_variable #unset it 
+echo "uninitialized_variale  = $uninitialized_variable"
+#It still has a null value 
+echo
