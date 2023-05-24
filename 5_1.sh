9915b91b19d1a0556a8f480cbd35c4b685946896
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-24 11:50:34 +0530

    Added the variable quoting

--- 5_1.sh
+++ 5_1.sh
@@ -3,7 +3,7 @@
 #Quoting Variables 
 # Escaping 
 
-#Quoting 
+#Quoting *************************************************************
 
 #Quoting means just , bracketing  a string in quotes.This has the effects of 
 #protecting special characters in the  String from reinterpretation or expansion by the shell or 
@@ -19,4 +19,23 @@
 # Note that the unquoted grep '[Ff]irst'*.txt works under the Bash shell.
 
 #Quoting can also supress echo's "appetite" for newlines.
-#Unless there is a file named first in the current working directory.Yet another reason to quote 
+#Unless there is a file named first in the current working directory.Yet another reason to quote
+
+#Quoting Variables ******************************************************
+
+#when referencing a variable it is generally advisable to enclose its name in the double quotes 
+# This prevents reinterpretation of all special characters within the quoted string --except $ ,`(backquote)
+# and \ (escape) . keeping $ as a special caracter within double quotes permits referencing a quoted
+# variable ("$variable") that is repalcing  the variable with its value 
+
+#Use double quotes to prevent word splitting .An argument enclosed in double quotes represent itself 
+# as  a single word even if it conatian whitespace seperators.
+
+#Enclosing the arguments to an echo statments in double Quotes is necessary only when word splitting
+# or preservaion of whitespace is an issue
+
+
+# Single Quotes ('') operate similarly to double quotes, but do not permit referencing  variables,
+# since the special meaning of $ is turned off.
+#  Within single quotes every special character expect gets interpreted literally 
+# Considet single quotes ("full quoting") to be the stricter method of quoting than double quotes 
--- 5_Example.sh
+++ 5_Example.sh
@@ -1,1 +1,81 @@
 #!/bin/bash
+
+List="one two three"
+
+for a in $List # Splits the variable in parts at whitespace.
+do 
+    echo "$a"
+done 
+#one
+#two 
+#three
+
+echo "----"
+
+for a in "$List" # Preserve whitespaces in an single variable.
+do #     ^     ^
+    echo "$a"
+done 
+# one two three
+
+
+#  A more elobrate example 
+variable1="a variable containing five words "
+
+COMMAND This is $variable1 # Executes COMMAND with 7 arguments:
+#"This" "is" "a" "variable" "containing" "five" "words"
+
+
+COMMAND "This is $variable1" # Executes COMMAND  with 1 arguments :
+#"This is a variable containing five words"
+
+variable2=""  #Empty
+
+COMMAND $variable2 $variable2 $variable2
+    #Execute COMMAND  with no arguments.
+
+COMMAND "$variable2" "$variable2" "$variable2"
+#   COMMAND executes with 3 empty arguments.
+
+COMMAND "$variable2 $variable2 $variable2"
+# COMMAND executes with 1  arguments (2 spaces).
+
+
+#echoing Weird Variables
+
+echo 
+var="'(]\\{}\$\""
+echo $var   # '(]\{}$"
+echo "$var" # '(]\{}$" Dosent make any difference 
+
+IFS='\'
+echo $var       #'(] {}$"     \ converted to space. Why?
+echo "$var"     #'(]\{}$"
+
+echo 
+var2="\\\\\""
+echo $var2  #   "
+echo "$var2"  # \\"
+echo 
+
+# But ......... var2="\\\\\"" is illegal Why?
+var3='\\\\'
+echo "$var3"  # \\\\
+# Strong Quoting works , though.
+
+#************************************************************####
+#As the first example above shows, nesting quotes is permitted.
+echo "$(echo '"')"     # "
+
+# At times this comes useful.
+
+var1="Two bits"
+echo "\$var1 = "$var1""           #$var1  = Two bits
+
+if [[ "$(du  "$My_File1")" -gt "$(du "$My_File2")"   ]]
+then 
+     echo "Ok boi"
+fi
+
+
+
