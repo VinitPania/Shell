9b41300918a56bb99152ddd322524bc4edb5df7c
VinitPania <vinit.pania@cloverinfotech.com>
2023-06-07 13:24:12 +0530

    Added some more theory and example of test construct

---
+++ 7_example.sh
@@ -0,0 +1,57 @@
+(( 0 && 1 )) #Logical AND
+echo $?  # 1
+
+
+let "num = (( 0 && 1 ))"
+echo $num # 0
+echo $? # 1
+
+(( 200 || 10 )) #Logical OR
+echo $? # 0 
+let "num = (( 100 || 10 ))"
+echo $num # 1
+echo $? # 0 
+
+(( 200 | 11 )) #Bitwise OR
+echo $? # 0
+let "num = (( 200 | 11 ))"
+echo $num # 203
+echo $?   # 0
+# The "let" construct retuns the same exit status as a double parenthesis arithmetic expression
+
+var = -2 && (( var+=2 ))
+echo $? # 1
+var=-2  && (( var+=2 )) && echo $var
+# Will not echo $var!
+
+
+
+# An If can test any command not just conditions enclosed within the brackets 
+
+if cmd a b &> /dev/null # Suppress output 
+then echo "Files a and b are identical."
+else echo "Files a n b differ" 
+fi 
+## the very useful "if-grep" construct":
+if grep -q Bash file 
+    then echo "File contains at least one occurence of Bash."
+fi 
+
+word=Linux 
+letter_sequence=inu
+if echo "$word" | grep -q "letter_sequence"
+# The "-q" option to grep suppress output.
+then 
+    echo "$letter_sequence found in $word"
+else 
+    echo "$letter_sequence not found in $word"
+fi 
+
+if COMMAND_WHOSE_EXIT_STATUS_IS_0_UNLESS_ERROR_OCCUREN
+    then 
+        echo "Command Succeeded"
+    else
+        echo "Command Failed"
+fi 
+
+
